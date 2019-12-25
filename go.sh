20cd2a12bf0e4b57f7acced00dc802a66dcfd37e
SNSLogty <1092352939@qq.com>
2019-12-25 23:40:28 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -14,18 +14,18 @@ surge_ruleset=🐟 漏网之鱼,[]FINAL
 ;设置规则标志位
 
 ;设置分组标志位
-custom_proxy_group=🔰 节点选择`select`[]✈ 手动选择`[]✈ 延迟最低`[]✈ 故障切换`[]DIRECT`(公告|地址)
-custom_proxy_group=📲 香港5倍`select`[]🔰 节点选择`[]✈ 手动选择`[]✈ 延迟最低`[]✈ 故障切换`(香港HKT-5倍)
-custom_proxy_group=📹 香港1倍`select`[]🔰 节点选择`[]✈ 手动选择`[]✈ 延迟最低`[]✈ 故障切换`(香港HKT-A|香港HKT-B|香港HGC)
-custom_proxy_group=🎥 台湾`select`[]🔰 节点选择`[]✈ 手动选择`[]✈ 延迟最低`[]✈ 故障切换`(台湾)
-custom_proxy_group=📺 美国`select`[]🔰 节点选择`[]✈ 手动选择`[]✈ 延迟最低`[]✈ 故障切换`(美国)
-custom_proxy_group=📺 日本`select`[]🔰 节点选择`[]✈ 手动选择`[]✈ 延迟最低`[]✈ 故障切换`(日本)
-custom_proxy_group=📺 新加坡`select`[]🔰 节点选择`[]✈ 手动选择`[]✈ 延迟最低`[]✈ 故障切换`(新加坡)
-custom_proxy_group=🌍 国外媒体`select`[]🔰 节点选择`[]✈ 手动选择`[]✈ 延迟最低`[]✈ 故障切换`[]DIRECT
+custom_proxy_group=🔰 节点选择`select`[]✈ 手动选择`[]✈ 延迟最低`[]DIRECT`(公告|地址)
+custom_proxy_group=📲 香港5倍`select`[]🔰 节点选择`[]✈ 手动选择`[]✈ 延迟最低`(香港HKT-5倍)
+custom_proxy_group=📹 香港1倍`select`[]🔰 节点选择`[]✈ 手动选择`[]✈ 延迟最低`(香港HKT-A|香港HKT-B|香港HGC)
+custom_proxy_group=🎥 台湾`select`[]🔰 节点选择`[]✈ 手动选择`[]✈ 延迟最低`(台湾)
+custom_proxy_group=📺 美国`select`[]🔰 节点选择`[]✈ 手动选择`[]✈ 延迟最低`(美国)
+custom_proxy_group=📺 日本`select`[]🔰 节点选择`[]✈ 手动选择`[]✈ 延迟最低`(日本)
+custom_proxy_group=📺 新加坡`select`[]🔰 节点选择`[]✈ 手动选择`[]✈ 延迟最低`(新加坡)
+custom_proxy_group=🌍 国外媒体`select`[]🔰 节点选择`[]✈ 手动选择`[]✈ 延迟最低`[]DIRECT
 custom_proxy_group=🌏 国内媒体`select`[]DIRECT`[]🔰 节点选择`[]✈ 手动选择`[]✈ 延迟最低
 custom_proxy_group=🍎 苹果服务`select`[]DIRECT`[]🔰 节点选择`[]✈ 手动选择`[]✈ 延迟最低`[]✈ 故障切换
 custom_proxy_group=🛑 全球拦截`select`[]REJECT`[]DIRECT
-custom_proxy_group=🐟 漏网之鱼`select`🔰 节点选择`select`[]✈ 手动选择`[]✈ 延迟最低`[]✈ 故障切换`[]DIRECT
+custom_proxy_group=🐟 漏网之鱼`select`🔰 节点选择`select`[]✈ 手动选择`[]✈ 延迟最低`[]DIRECT
 custom_proxy_group=✈ 手动选择`select`.*
 custom_proxy_group=✈ 延迟最低`url-test`.*`http://www.gstatic.com/generate_204`500
 ;设置分组标志位
